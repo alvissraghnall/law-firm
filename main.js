6e750ae615d6e0e1153c6c82c801e7d310b0b7e3
Alviss Raghnall <isiomastewart@gmail.com>
2023-10-26 03:47:34 +0100

    added mission section (desktop) ++ remaining play icon

--- main.js
+++ main.js
@@ -26,12 +26,21 @@ let hamburgerMenuOpen = false;
 
 
 const hamburgerIconEl = document.getElementById('hamburgerIcon');
+const closeMobileMenuIconEl = document.getElementById('closeMobileMenuIcon');
 const mobileMenuEl = document.getElementById('mobileMenu');
 
 hamburgerIconEl.addEventListener('click', () => {
     console.log(hamburgerMenuOpen);
     hamburgerMenuOpen = !hamburgerMenuOpen
     mobileMenuEl.classList.toggle('!hidden');
+
+    if (hamburgerMenuOpen === false) {
+        hamburgerIconEl.innerHTML = `<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />`;
+        // closeMobileMenuIconEl.classList.add("hidden");
+    } else {
+        // closeMobileMenuIconEl.classList.remove("hidden");
+        hamburgerIconEl.innerHTML = `<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />`;
+    }
 });
 
 setupCounter(document.querySelector('#counter'))
