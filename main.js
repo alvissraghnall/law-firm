eff24847f25c557b7074f190df597e1db629e8d9
Alviss Raghnall <isiomastewart@gmail.com>
2023-11-05 01:46:04 +0100

    sjee

--- main.js
+++ main.js
@@ -3,7 +3,52 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js';
 
+
+const hamburgerIconEl = document.getElementById('hamburgerIcon');
+const sliderHolderEl = document.getElementById('sliderHolder');
+const closeMobileMenuIconEl = document.getElementById('closeMobileMenuIcon');
+const mobileMenuEl = document.getElementById('mobileMenu');
+const slides = document.querySelectorAll(".slide");
+const indicators = document.querySelectorAll(".indicator");
+
 let hamburgerMenuOpen = false;
+let slideIndex = 0;
+
+const handleClick = (direction) => {
+    direction === "left" ? slideIndex = slideIndex > 0 ? slideIndex - 1 : 6 : slideIndex = slideIndex < 6 ? slideIndex + 1 : 0;
+};
+
+const showSlide = idx => {
+    slides.forEach((slide, i) => {
+        // const transformVal = `translateX(${100 * (i - idx)}%)`;
+        if (i === idx) {
+            slide.classList.remove("-translate-x-full");
+            slide.style.display = 'block';
+        }
+        else {
+            slide.classList.add("-translate-x-full");
+            slide.style.display = 'none';
+        }
+        // slide.style.transform = transformVal;
+    });
+
+    indicators.forEach((indicator, i) => {
+        // sliderHolderEl.style.transform = `translateX(${slideIndex * -100}vw)`;
+
+        if (i === idx) indicator.classList.add('active');
+        else indicator.classList.remove('active');
+    });
+}
+
+indicators.forEach((indicator, i) => {
+    indicator.addEventListener('click', () => {
+        slideIndex = i;
+        showSlide(slideIndex);
+    });
+});
+
+showSlide(slideIndex);
+
 
 // document.querySelector('#app').innerHTML = `
 //   <div>
@@ -24,11 +69,6 @@ let hamburgerMenuOpen = false;
 // `
 
 
-
-const hamburgerIconEl = document.getElementById('hamburgerIcon');
-const closeMobileMenuIconEl = document.getElementById('closeMobileMenuIcon');
-const mobileMenuEl = document.getElementById('mobileMenu');
-
 hamburgerIconEl.addEventListener('click', () => {
     console.log(hamburgerMenuOpen);
     hamburgerMenuOpen = !hamburgerMenuOpen
@@ -41,6 +81,12 @@ hamburgerIconEl.addEventListener('click', () => {
         // closeMobileMenuIconEl.classList.remove("hidden");
         hamburgerIconEl.innerHTML = `<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />`;
     }
+});
+
+
+
+window.addEventListener("DOMContentLoaded", ev => {
+    sliderHolderEl.style.transform = `translateX(${slideIndex * -100}vw)`;
 });
 
 setupCounter(document.querySelector('#counter'))
