0253775cda7be022b20f3ff445e97a7c7cc8b8d4
Alviss Raghnall <isiomastewart@gmail.com>
2023-11-18 13:51:37 +0100

    done.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,22 @@ export default {
       },
       fontFamily: {
         "rubik": ["Rubik", ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        'sm': '640px',
+        'md': '838px',
+        'lg': '960px',
+        'xl': '1200px',
+        '2xl': '1500px',
+      }, 
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "100" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.3s ease-in-out forwards"
       }
     },
   },
